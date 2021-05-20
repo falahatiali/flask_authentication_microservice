5757f2e03056d46f2c3abccd66167097bb478378
Ali Falahati <ali.falahati.2010@gmail.com>
2021-05-20 15:22:38 +0430

    add User Model and its related libraries in main package file

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 
 from authz.config import Config
 
-api=Api()
+api = Api()
+db  = SQLAlchemy()
 
 from authz import resource
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) #Config loads
+    db.init_app(app)
     api.init_app(app)
     return app
