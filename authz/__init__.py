2d6f3458f7657885335c5f5b5bc3b04d32ccf9b7
Ali Falahati <ali.falahati.2010@gmail.com>
2021-05-20 21:30:46 +0430

    add sql-migrate package. adding to project init file. start migration

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 from authz.config import Config
 
 api = Api()
 db  = SQLAlchemy()
+mg  = Migrate()
+
 
 from authz import resource
 
@@ -14,6 +16,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) #Config loads
     db.init_app(app)
+    mg.init_app(app , db)
     api.init_app(app)
-    print(Config.SQLALCHEMY_DATABASE_URI)
     return app
