2fa61fb46a309555a7ca86449c0ae4de7ed2176c
Ali Falahati <ali.falahati.2010@gmail.com>
2021-05-20 15:14:49 +0430

    add Configs related to User model

--- authz/config/config.py
+++ authz/config/config.py
@@ -6,3 +6,7 @@ class Config:
     ENV = environ.get("MICRO_AUTH_ENV", "production")
     DEBUG = int(environ.get("MICRO_AUTH_DEBUG", "0"))
     TESTING = int(environ.get("MICRO_AUTH_TESTING", "0"))
+
+    ##################### USER CONFIGURATIONS #####################
+    USER_DEFAULT_ROLE = environ.get("MICRO_AUTH_DEFAULT_ROLE", "member")
+    USER_DEFAULT_STATUS = environ.get("MICRO_AUTH_DEFAULT_STATUS", "inactive")
