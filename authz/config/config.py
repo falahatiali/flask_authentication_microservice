45e46ed5b17d157675e94d7f59a26703797d85b3
Ali Falahati <ali.falahati.2010@gmail.com>
2021-05-23 00:28:55 +0430

    Add JWT token generation

--- authz/config/config.py
+++ authz/config/config.py
@@ -7,6 +7,10 @@ class Config:
     DEBUG = int(environ.get("MICRO_AUTH_DEBUG", "0"))
     TESTING = int(environ.get("MICRO_AUTH_TESTING", "0"))
 
+    AUTHZ_SECRET = environ.get("MICRO_AUTH_SECRET","HARD-SECURE-@-123-CODE-A#FERDFRQWEUYTRGCSS")
+    JWT_ALGO = environ.get("MICRO_AUTH_JWT_ALGO" , "HS512")
+    JWT_TOKEN_LIFETIME = int(environ.get("MICRO_AUTH_JWT_LIFETIME" , "86400"))
+
     ##################### USER CONFIGURATIONS #####################
     USER_DEFAULT_ROLE = environ.get("MICRO_AUTH_DEFAULT_ROLE", "member")
     USER_DEFAULT_STATUS = environ.get("MICRO_AUTH_DEFAULT_STATUS", "inactive")
@@ -15,3 +19,4 @@ class Config:
     ##################### USER CONFIGURATIONS #####################
     SQLALCHEMY_TRACK_MODIFICATIONS = TESTING
     SQLALCHEMY_DATABASE_URI = environ.get('MICRO_AUTH_DATABASE_URI', None)
+    
--- authz/resource/__init__.py
+++ authz/resource/__init__.py
@@ -1,5 +1,13 @@
 from authz import api
 from authz.resource.user import UserResource
+from authz.resource.auth import AuthTokenResource
+
+api.add_resource(
+    AuthTokenResource,
+    "/auth/tokens",
+    methods=["post"],
+    endpoint="auth_tokens"
+)
 
 api.add_resource(
     UserResource , 
