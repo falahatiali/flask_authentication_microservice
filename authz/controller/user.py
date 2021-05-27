fb0363e519e362e14bc01a988df0c46daea82c97
Ali Falahati <ali.falahati.2010@gmail.com>
2021-05-27 20:50:28 +0430

    Add simple ACL to system. authorize user and requests

--- authz/controller/user.py
+++ authz/controller/user.py
@@ -1,23 +1,23 @@
-from flask import request,abort
+from flask import request, abort
 from authz import db
 from authz.model import User
 from authz.schema import UserSchema
-
+from authz.decorator import auth_required
 
 class UserController:
     def create_user():
         if request.content_type != 'application/json':
             abort(415)
-        
+
         user_schema = UserSchema(only=["username", "password"])
         try:
             data = user_schema.load(request.get_json())
         except:
             abort(400)
-        
+
         if not data["username"] or not data["password"]:
             abort(400)
-        
+
         try:
             user = User.query.filter_by(username=data["username"]).first()
         except:
@@ -30,18 +30,18 @@ class UserController:
         db.session.add(user)
 
         try:
-            db.session.commit() #Database create query
+            db.session.commit()  # Database create query
         except:
             db.session.rollback()
             abort(500)
-        
+
         user_schema = UserSchema()
 
         return {
-            "user": user_schema.dump(user)
-        } ,201
+                   "user": user_schema.dump(user)
+               }, 201
 
-    
+    @auth_required
     def get_users():
         try:
             users = User.query.all()
@@ -50,10 +50,10 @@ class UserController:
 
         user_schema = UserSchema(many=True)
         return {
-            "users": user_schema.dump(users)
-        } ,200
+                   "users": user_schema.dump(users)
+               }, 200
 
-
+    @auth_required
     def get_user(user_id):
         try:
             user = User.query.get(user_id)
@@ -62,14 +62,14 @@ class UserController:
 
         user_schema = UserSchema()
         return {
-            "user": user_schema.dump(user)
-        } ,200
+                   "user": user_schema.dump(user)
+               }, 200
 
-
+    @auth_required
     def update_user(user_id):
         if request.content_type != 'application/json':
             abort(415)
-        
+
         user_schema = UserSchema(only=["password"])
         try:
             data = user_schema.load(request.get_json())
@@ -79,27 +79,26 @@ class UserController:
             abort(400)
 
         try:
-            user = User.query.get(user_id) 
+            user = User.query.get(user_id)
         except:
             abort(500)
 
         if user is None:
-           abort(404)
-        
+            abort(404)
+
         user.password = data["password"]
         try:
-            db.session.commit() #Database UPDATE Query
+            db.session.commit()  # Database UPDATE Query
         except:
             db.session.rollback()
-            abort(500) #database error
+            abort(500)  # database error
 
         user_schema = UserSchema()
         return {
-            "user": user_schema.dump(user)
-        }, 200
+                   "user": user_schema.dump(user)
+               }, 200
 
-        
-
+    @auth_required
     def delete_user(user_id):
         try:
             user = User.query.get(user_id)
@@ -109,11 +108,10 @@ class UserController:
         if user is None:
             abort(404)
         try:
-            db.session.delete(user) # Database delete query
+            db.session.delete(user)  # Database delete query
         except:
             db.session.rollback()
-            abort(500) # Database error
+            abort(500)  # Database error
 
         db.session.commit()
         return "", 204
-        
